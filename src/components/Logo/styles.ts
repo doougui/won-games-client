a76949d40d62e942c48b61ec0f24ba4036edd35d
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-17 20:28:50 -0300

    feat: hiding logo text when on mobile if hideOnMobile prop is true

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components'
 import { LogoProps } from '.'
+import media from 'styled-media-query'
 
 const wrapperModifiers = {
   normal: () => css`
@@ -9,13 +10,30 @@ const wrapperModifiers = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
 
     ${!!size && wrapperModifiers[size]};
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile};
   `}
 `
