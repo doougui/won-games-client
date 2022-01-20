5a5256db702e9a0c146c8c99f8e62089625e780d
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-19 21:42:33 -0300

    feat: allowing to pass an icon to the button component

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,4 +1,5 @@
 import Button from '.'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
 export default {
@@ -7,6 +8,9 @@ export default {
   argTypes: {
     children: {
       type: 'string'
+    },
+    icon: {
+      type: 'symbol'
     }
   }
 } as Meta
@@ -15,4 +19,12 @@ export const Default: Story = (args) => <Button {...args} />
 
 Default.args = {
   children: 'Buy now'
+}
+
+export const WithIcon: Story = (args) => <Button {...args} />
+
+WithIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />
 }
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,17 +1,20 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 import Button from '.'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '4rem',
       'font-size': '1.4rem',
       padding: '0.8rem 3.2rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the small size', () => {
@@ -31,5 +34,22 @@ describe('<Button />', () => {
       'font-size': '1.6rem',
       padding: '0.8rem 4.8rem'
     })
+  })
+
+  it('should render a full width version', () => {
+    renderWithTheme(<Button fullWidth>Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      width: '100%'
+    })
+  })
+
+  it('should render an icon version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+
+    expect(screen.getByText(/Buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId(/icon/i)).toBeInTheDocument()
   })
 })
