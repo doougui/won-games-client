e3a18e9bf68dd6e670f04c620f13109425e04ab3
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-19 21:12:16 -0300

    tests: creating tests for button component

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -4,9 +4,32 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
+  it('should render the medium size by default', () => {
     renderWithTheme(<Button>Buy now</Button>)
 
-    expect(screen.getByRole('button', { name: /Buy now/i })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '4rem',
+      'font-size': '1.4rem',
+      padding: '0.8rem 3.2rem'
+    })
+  })
+
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '5rem',
+      'font-size': '1.6rem',
+      padding: '0.8rem 4.8rem'
+    })
   })
 })
