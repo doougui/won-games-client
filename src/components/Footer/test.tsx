e0d12bab8760e3bfb40227f7d0af5690b3e01a4b
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-24 20:44:44 -0300

    feat: creating footer component

--- src/components/Footer/test.tsx
+++ src/components/Footer/test.tsx
@@ -1,12 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Footer from '.'
 
 describe('<Footer />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Footer />)
+  it('should render the logo in the footer', () => {
+    renderWithTheme(<Footer />)
 
-    expect(screen.getByRole('heading', { name: /Footer/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should render 4 columns topics', () => {
+    const { container } = renderWithTheme(<Footer />)
+
+    expect(
+      screen.getByRole('heading', { name: /contact/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /location/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
