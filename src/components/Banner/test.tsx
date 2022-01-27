1824ada33a0e50d5db05993f709e3a89f438b7da
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-27 20:21:12 -0300

    feat: creating banner component initial structure

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(
+      <Banner
+        img="https://source.unsplash.com/user/willianjusten/1042x580"
+        title="Defy death"
+        subtitle="<p>Play the new <strong>CrashLands</strong> season"
+        buttonLabel="Buy now"
+        buttonLink="/games/defy-death"
+      />
+    )
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Play the new CrashLands season/i
+      })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toBeInTheDocument()
+  })
+})
