9b5e288573eb0f764b600fe52b78dd23ed1c96ee
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-29 20:18:30 -0300

    feat: creating ribbon component

---
+++ src/components/Ribbon/test.tsx
@@ -0,0 +1,46 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Ribbon from '.'
+
+describe('<Ribbon />', () => {
+  it('should render the text corretly', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toBeInTheDocument()
+  })
+
+  it('should render with the primary color', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      'background-color': '#F231A5'
+    })
+  })
+
+  it('should render with the secondary color', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      'background-color': '#3CD3C1'
+    })
+  })
+
+  it('should render with the normal size by default', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      height: '3.6rem',
+      fontSize: '1.4rem'
+    })
+  })
+
+  it('should render with the small size', () => {
+    renderWithTheme(<Ribbon size="small">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
+})
