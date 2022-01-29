63305fcbd246ae9379cf338aa71d7398f0587c6a
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-29 20:56:12 -0300

    feat: creating initial structure of highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,15 @@
+import Highlight, { HighlightProps } from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Red Dead is back',
+    subtitle: 'An epic tale of honor and loyalty at the dawn of the modern age',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
