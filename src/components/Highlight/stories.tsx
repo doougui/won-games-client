ce94ed13814cd72072c46f97c90706dc29c3efd3
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-29 22:27:57 -0300

    feat: allowing to pass float image to highlight component

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,16 +1,30 @@
-import Highlight, { HighlightProps } from '.'
 import { Story, Meta } from '@storybook/react/types-6-0'
+import Highlight, { HighlightProps } from '.'
 
 export default {
   title: 'Highlight',
   component: Highlight,
   args: {
-    title: 'Red Dead is back',
-    subtitle: 'An epic tale of honor and loyalty at the dawn of the modern age',
+    title: 'Read Dead is back!',
+    subtitle: 'Come see John’s new adventures',
     backgroundImage: '/img/red-dead-img.jpg',
     buttonLabel: 'Buy now',
-    buttonLink: '/rdr2'
+    buttonLink: '/games/rdr2'
   }
 } as Meta
 
-export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
