85e3408afb38b4e6ace56dff27e22c68c849d85c
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-02-10 21:19:25 -0300

    feat: creating checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Checkbox = () => (
+  <S.Wrapper>
+    <label>
+      <input id="action" type="checkbox" />
+      Action
+    </label>
+  </S.Wrapper>
+)
+
+export default Checkbox
---
+++ src/components/Checkbox/stories.tsx
@@ -0,0 +1,9 @@
+import Checkbox from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Checkbox',
+  component: Checkbox
+} as Meta
+
+export const Default: Story = () => <Checkbox />
