6b153cac3df6cb7f530fadf48d85f66ad40b99fd
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-02-12 14:06:19 -0300

    feat: adding onChange method to checkbox

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -3,7 +3,10 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Checkbox',
-  component: Checkbox
+  component: Checkbox,
+  argTypes: {
+    onCheck: { action: 'checked' }
+  }
 } as Meta
 
 export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
