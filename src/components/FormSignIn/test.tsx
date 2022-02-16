acaca9ad5a74020f69c26f71a544623201faa667
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-02-15 21:51:50 -0300

    feat: creating styles of sign in form

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -5,7 +5,7 @@ import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    renderWithTheme(<FormSignIn />)
+    const { container } = renderWithTheme(<FormSignIn />)
 
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
@@ -13,6 +13,8 @@ describe('<FormSignIn />', () => {
     expect(
       screen.getByRole('button', { name: /sign in now/i })
     ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the forgot password link', () => {
