ada7ab5c28472186f549bd4e2e3444476f87a703
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-12 18:11:16 -0300

    feat: creating GameInfo styling

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,9 +1,20 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import GameInfo, { GameInfoProps } from '.'
+import mockGame from './mock'
 
 export default {
   title: 'GameInfo',
-  component: GameInfo
+  component: GameInfo,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame
 } as Meta
 
-export const Default: Story<GameInfoProps> = (args) => <GameInfo {...args} />
+export const Default: Story<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: '144rem', margin: 'auto', padding: '1.5rem' }}>
+    <GameInfo {...args} />
+  </div>
+)
