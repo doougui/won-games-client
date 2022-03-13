78d41645be2910d5d20d57ea098f1e82ad58dded
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-13 16:52:26 -0300

    feat: adjusting GameDetails story

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,6 +1,8 @@
 import GameDetails, { GameDetailsProps } from '.'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
+import mockGame from './mock'
+
 export default {
   title: 'Game/GameDetails',
   component: GameDetails,
@@ -9,14 +11,23 @@ export default {
       default: 'won-dark'
     }
   },
-  args: {
-    platforms: ['windows', 'linux', 'mac']
-  },
+  args: mockGame,
   argTypes: {
+    releaseDate: {
+      control: {
+        type: 'date'
+      }
+    },
     platforms: {
       control: {
         type: 'inline-check',
         options: ['windows', 'linux', 'mac']
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative', 'Action']
       }
     }
   }
