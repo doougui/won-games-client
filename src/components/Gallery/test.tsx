81e5ff1ca334f692d21cb5a87ba0031154997ad4
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-14 19:55:55 -0300

    feat: creating tests for gallery and creating modal open method

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,10 +1,44 @@
+import { fireEvent, screen } from '@testing-library/react'
 import 'match-media-mock'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Gallery from '.'
 
+import mockItems from './mock'
+
 describe('<Gallery />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Gallery items={[]} />)
+  it('should render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute('src', mockItems[0].src)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    ).toHaveAttribute('src', mockItems[1].src)
+  })
+
+  it('should handle open/close modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // Select the modal
+    const modal = screen.getByLabelText('modal')
+
+    // Verifiy if the menu full is hidden
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
+
+    // Click the open menu button and assert that it's open and not hidden
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+
+    // Click the close menu button and assert that it's closed and hidden
+    // fireEvent.click(screen.getByLabelText(/close menu/i))
+    // expect(modal.getAttribute('aria-hidden')).toBe('true')
+    // expect(modal).toHaveStyle({ opacity: 0 })
   })
 })
