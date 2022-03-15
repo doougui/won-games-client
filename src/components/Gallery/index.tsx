f2b8025da75bd2500d83719387394f2321538df1
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-15 20:17:47 -0300

    feat: creating gallery modal close method and creating overlay

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
 import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
+import { Close } from '@styled-icons/material-outlined/Close'
 
 import Slider, { SliderSettings } from 'components/Slider'
 
@@ -67,11 +68,15 @@ const Gallery = ({ items }: GalleryProps) => {
         ))}
       </Slider>
 
-      <S.Modal
-        isOpen={isOpen}
-        aria-label="modal"
-        aria-hidden={!isOpen}
-      ></S.Modal>
+      <S.Modal isOpen={isOpen} aria-label="modal" aria-hidden={!isOpen}>
+        <S.Close
+          role="button"
+          aria-label="Close modal"
+          onClick={() => setIsOpen(false)}
+        >
+          <Close size={40} />
+        </S.Close>
+      </S.Modal>
     </S.Wrapper>
   )
 }
--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -25,20 +25,32 @@ describe('<Gallery />', () => {
     // Select the modal
     const modal = screen.getByLabelText('modal')
 
-    // Verifiy if the menu full is hidden
+    // Verifiy if the modal is hidden
     expect(modal.getAttribute('aria-hidden')).toBe('true')
     expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
 
-    // Click the open menu button and assert that it's open and not hidden
+    // Click the open thumb and assert that the modal is open and not hidden
     fireEvent.click(
       screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
     )
     expect(modal.getAttribute('aria-hidden')).toBe('false')
     expect(modal).toHaveStyle({ opacity: 1 })
+  })
 
-    // Click the close menu button and assert that it's closed and hidden
-    // fireEvent.click(screen.getByLabelText(/close menu/i))
-    // expect(modal.getAttribute('aria-hidden')).toBe('true')
-    // expect(modal).toHaveStyle({ opacity: 0 })
+  it('should handle close modal when overlay or button clicked', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // Select the modal
+    const modal = screen.getByLabelText('modal')
+
+    // Click the open thumb and assert that the modal is open and not hidden
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+
+    // Click to close the modal
+    fireEvent.click(screen.getByRole('button', { name: /close modal/i }))
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
   })
 })
