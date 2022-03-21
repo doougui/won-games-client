2b7a47d50c3804e52acf14f5ca61ce90ba7085ee
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-21 20:14:45 -0300

    feat: getting game cover img via getStaticProps and getStaticPaths

--- src/templates/Game/test.tsx
+++ src/templates/Game/test.tsx
@@ -4,6 +4,8 @@ import Game from '.'
 
 describe('<Game />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<Game />)
+    renderWithTheme(
+      <Game cover="https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg" />
+    )
   })
 })
