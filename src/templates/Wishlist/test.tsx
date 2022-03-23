0a4c5ba015cd133d39ef3b948b6b49aa19ee8e49
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-23 18:55:01 -0300

    feat: creating recommended games showcase section in wishlist page

--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -1,9 +1,19 @@
+import 'match-media-mock'
+
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Wishlist from '.'
 
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props = {
+  recommendedGames: gamesMock,
+  recommendedHighlight: highlightMock
+}
+
 describe('<Wishlist />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<Wishlist />)
+    renderWithTheme(<Wishlist {...props} />)
   })
 })
