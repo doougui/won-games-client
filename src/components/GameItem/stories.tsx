09b7a7cb7b51804c96fafb82893a63187356065b
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-24 18:51:26 -0300

    feat: creating GameItem component

---
+++ src/components/GameItem/stories.tsx
@@ -0,0 +1,14 @@
+import GameItem, { GameItemProps } from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'GameItem',
+  component: GameItem,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00'
+  }
+} as Meta
+
+export const Default: Story<GameItemProps> = (args) => <GameItem {...args} />
