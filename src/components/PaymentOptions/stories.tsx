2fe7f4d3f110efe052ab1f5436da9efa395bf30c
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-29 20:01:01 -0300

    feat: adding styles and tests of PaymentOptions component

--- src/components/PaymentOptions/stories.tsx
+++ src/components/PaymentOptions/stories.tsx
@@ -1,5 +1,5 @@
-import PaymentOptions, { PaymentOptionsProps } from '.'
 import { Story, Meta } from '@storybook/react/types-6-0'
+import PaymentOptions, { PaymentOptionsProps } from '.'
 
 import cardsMock from './mock'
 
@@ -8,9 +8,24 @@ export default {
   component: PaymentOptions,
   args: {
     cards: cardsMock
+  },
+  argTypes: {
+    cards: {
+      type: 'symbol'
+    },
+    handlePayment: {
+      action: 'clicked'
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
   }
 } as Meta
 
 export const Default: Story<PaymentOptionsProps> = (args) => (
-  <PaymentOptions {...args} />
+  <div style={{ padding: 16, maxWidth: 400 }}>
+    <PaymentOptions {...args} />
+  </div>
 )
--- src/components/PaymentOptions/test.tsx
+++ src/components/PaymentOptions/test.tsx
@@ -1,16 +1,15 @@
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import PaymentOptions from '.'
-
-import cardsMock from './mock'
-
-const props = {
-  cards: cardsMock,
-  handlePayment: jest.fn()
-}
+import cards from './mock'
 
 describe('<PaymentOptions />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<PaymentOptions {...props} />)
+  it('should render the saved card options and the add new card button', () => {
+    renderWithTheme(<PaymentOptions cards={cards} handlePayment={jest.fn} />)
+
+    expect(screen.getByLabelText(/4325/)).toBeInTheDocument()
+    expect(screen.getByLabelText(/4326/)).toBeInTheDocument()
+    expect(screen.getByText(/add a new credit card/i)).toBeInTheDocument()
   })
 })
