d5ca856c8b044b7c6a4c8b6f845f1f5c90df763f
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-29 20:50:18 -0300

    feat: creating Cart's empty state

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,6 +1,7 @@
 import CartList, { CartListProps } from 'components/CartList'
 import { Container } from 'components/Container'
 import { Divider } from 'components/Divider'
+import Empty from 'components/Empty'
 import { GameCardProps } from 'components/GameCard'
 import Heading from 'components/Heading'
 import { HighlightProps } from 'components/Highlight'
@@ -32,11 +33,19 @@ const Cart = ({
           My cart
         </Heading>
 
-        <S.Content>
-          <CartList items={items} total={total} />
+        {items?.length ? (
+          <S.Content>
+            <CartList items={items} total={total} />
 
-          <PaymentOptions cards={cards} handlePayment={handlePayment} />
-        </S.Content>
+            <PaymentOptions cards={cards} handlePayment={handlePayment} />
+          </S.Content>
+        ) : (
+          <Empty
+            title="Your cart is empty"
+            description="Go back to the store and explore great games and offers"
+            hasLink
+          />
+        )}
         <Divider />
       </Container>
 
