c04568a307f362c14ab4de5caeb5c05ec0a5ebc4
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-31 19:19:17 -0300

    feat: creating Profile page and template

---
+++ src/pages/profile/cards.tsx
@@ -0,0 +1,10 @@
+import CardsList from 'components/CardsList'
+import Profile from 'templates/Profile'
+
+export default function CardsPage() {
+  return (
+    <Profile>
+      <CardsList />
+    </Profile>
+  )
+}
