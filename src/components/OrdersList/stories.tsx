2f76f7c3a45839499c54f7137b4967dcf02398f4
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-03-31 19:50:20 -0300

    feat: creating OrdersList component

---
+++ src/components/OrdersList/stories.tsx
@@ -0,0 +1,18 @@
+import OrdersList, { OrdersListProps } from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import itemsMock from './mock'
+
+export default {
+  title: 'Profile/OrdersList',
+  component: OrdersList,
+  args: {
+    items: itemsMock
+  }
+} as Meta
+
+export const Default: Story<OrdersListProps> = (args) => (
+  <div style={{ maxWidth: 850, margin: 'auto' }}>
+    <OrdersList {...args} />
+  </div>
+)
