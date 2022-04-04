b30ecc7c7e472f6e60fd0b7d3693a53c07f521a8
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-04-04 19:30:22 -0300

    feat: updating menu links

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -3,14 +3,19 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Menu',
-  component: Menu
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
 export const Default: Story<MenuProps> = (args) => <Menu {...args} />
 
-Default.parameters = {
-  layout: 'fullscreen',
-  backgrounds: {
-    default: 'won-dark'
-  }
+export const Logged: Story<MenuProps> = (args) => <Menu {...args} />
+
+Logged.args = {
+  username: 'John Doe'
 }
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -10,7 +10,8 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+    // expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+    expect(screen.getAllByLabelText(/shopping cart/i)).toHaveLength(2)
   })
 
   it('should handle the open/close mobile menu', () => {
@@ -37,7 +38,7 @@ describe('<Menu />', () => {
   it('should show register box when logged out', () => {
     renderWithTheme(<Menu />)
 
-    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/my profile/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
 
     expect(screen.getByText(/sign in now/i)).toBeInTheDocument()
@@ -50,7 +51,7 @@ describe('<Menu />', () => {
     expect(screen.queryByText(/sign in now/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
 
-    expect(screen.getByText(/my account/i)).toBeInTheDocument()
-    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/my profile/i)).toHaveLength(2)
+    expect(screen.getAllByText(/wishlist/i)).toHaveLength(2)
   })
 })
