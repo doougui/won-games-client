373aec005b824dedebe0a79a650becfd3725d3c0
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-04-04 20:43:27 -0300

    feat: adding apollo client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,24 @@ import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'https://wongamesapi.herokuapp.com/graphql',
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `
+  })
+
   return <Home {...props} />
 }
 
