08ed1ec0159f1078b4038f123618d688a27cb12a
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-04-07 20:05:58 -0300

    feat: fetching banners data from api instead of mock

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,36 @@
+import { initializeApollo } from 'utils/apollo'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      banners: data.banners.map((banner) => ({
+        img: banner.image?.url,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon?.text,
+          ribbonColor: banner.ribbon?.color,
+          ribbonSize: banner.ribbon?.size
+        })
+      })),
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
@@ -20,6 +39,7 @@ export async function getServerSideProps() {
       upcomingMoreGames: gamesMock,
       freeGames: gamesMock,
       freeHighlight: highlightMock
-    }
+    },
+    revalidate: 10
   }
 }
