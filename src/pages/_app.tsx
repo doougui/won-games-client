ddb72e969b558cae695b08308beec5216f31fc73
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-04-20 20:35:59 -0300

    feat: creating custom render function for tests

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 // import App from "next/app";
 import { ApolloProvider } from '@apollo/client'
+import { CartProvider } from 'hooks/use-cart'
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
@@ -13,15 +14,20 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-192.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="description" content="The best game store in the world" />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <CartProvider>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-192.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="The best game store in the world"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </CartProvider>
       </ThemeProvider>
     </ApolloProvider>
   )
